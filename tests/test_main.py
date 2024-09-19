c7b2140e0a7571133fd1a19af8b99d9a038dd641
andrestovio <act@njit.edu>
2024-09-19 17:08:27 -0400

    added division and testing for fivide by zero

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,6 @@
 '''My Calculator Test'''
-from app.main import addition, multiplication, subtraction
+import pytest
+from app.main import addition, division, multiplication, subtraction
 
 def test_addition():
     '''Addition function'''
@@ -12,4 +13,12 @@ def test_subtraction():
 def test_multiplication():
     '''Multiplication function'''
     assert multiplication(1,2) == 2
-    
+
+def test_positivedivision():
+    '''Positive Division Test'''
+    assert division(1,1) == 1
+
+def test_negativedivision():
+    '''Negative Division Test'''
+    with pytest.raises(ZeroDivisionError):
+        division(10,0)
